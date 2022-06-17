878d55f46b61f492d58ac630201823a287776e80
anomiex <anomiex@users.noreply.github.com>
2022-06-17 14:46:24 +0000

    js/components: Use jest for tests (#24762)
    Convert from js-test-runner/mocha/chai/enzyme to jest and
    @testing-library. Add linting of the tests too.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2516134151

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd39c8924cc0f5636e01b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '882177d42383729d7f27');
