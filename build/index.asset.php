2c095108474a14bb0518064dbfe3d2ffde4b37db
oskosk <oskosk@users.noreply.github.com>
2022-06-22 11:34:48 +0000

    JS Components: Fix multiline visual issue in Alert component (#24788)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2541863958

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '55eb77dc35166559f513');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '13df6488ae61b6876a21');
