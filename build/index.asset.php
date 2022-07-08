499ceeeb4978f770e848f5cb40ba3c3367993249
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-07-08 14:45:23 +0000

    JS Components: Convert ProductPrice component to TypeScript  (#24931)
    * Extract Price component to separate file
    
    * Convert ProductPrice component to TypeScript
    
    * Convert ProductPrice stories to TypeScript
    
    * changelog
    
    * Fix versions
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2636716087

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '409d96a6d4dcb288b593');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'be16a37c681c1e58df01');
