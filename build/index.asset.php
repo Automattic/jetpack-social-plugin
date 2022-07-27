6d82432e1a7fd657f3031e8bc1fabb1a92d4910c
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-07-27 11:48:44 +0000

    JS Components: Convert AutomatticBylineLogo component to TypeScript (#24944)
    * Convert AutomatticBylineLogo logo to TypeScript
    
    * Convert AutomatticBylineLogo tests to TypeScript
    
    * Add more test cases
    
    * Add changelog
    
    * Convert stories to TypeScript
    
    * Fix version
    
    * Fix doc block
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2746335329

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '553de0042815847a3888');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '368f55893f53704d3a41');
