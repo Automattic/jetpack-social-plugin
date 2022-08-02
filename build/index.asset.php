b7db379615521914f0e604aa5a32823ce81a1741
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-08-02 18:16:23 +0000

    JS Components: Convert ProductOffer component to TypeScript (#25294)
    * [not verified] Fix TS types for button
    
    * [not verified] Extract re-usable components to separate files
    
    * [not verified] Convert ProductOffer component to TypeScript
    
    * [not verified] Convert ProductOffer stories to TypeScript
    
    * [not verified] Update types.ts
    
    * [not verified] Fix story source not loading
    
    * Fix and improve types
    
    * Add changelog
    
    * Fix type errors
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2784211310

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a184494aba32d8fd015d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c6953880383724bc98d1');
