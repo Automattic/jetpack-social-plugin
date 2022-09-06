e1c4e7c1afe34010b3d7ae0c966f32fca39be70a
sdixon194 <sdixon194@users.noreply.github.com>
2022-09-06 21:40:50 +0000

    Social: Enable Autotag, Autorelease and Autosvn (#26076)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3003495872

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -28,10 +28,6 @@ return array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
       'ver' => '2.2.26',
     ),
-    'jetpack-my-jetpack' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.0.4',
-    ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
       'ver' => '1.9.6',
@@ -47,6 +43,10 @@ return array(
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
       'ver' => '0.8.22',
+    ),
+    'jetpack-my-jetpack' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
+      'ver' => '2.0.4',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
