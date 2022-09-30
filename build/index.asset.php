ad549afc8ec5d1a560581c86b118fe4008cc0195
oskosk <oskosk@users.noreply.github.com>
2022-09-30 21:03:28 +0000

    Improve PricingTable component items (#26364)
    Co-authored-by: retrofox <rdsuarez@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3161580719

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9ff99ef86f0f9f563c5f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9e9de94ecb5d297491e2');
