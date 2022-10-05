e1973d302893ad2bece3e2a9c05a932803bc98e2
jsnmoon <jsnmoon@users.noreply.github.com>
2022-10-05 06:56:13 +0000

    Search: Translate copy for the new pricing table (#26595)
    * Internationalize untranslated strings
    
    * changelog
    
    * Adjust some i18n and bold styles
    
    * Fix ProductPrice layout for long prices
    
    * changelog
    
    * Fix translator comments
    
    * Remove unnecessary !important
    
    Co-authored-by: Dognose <dognose24@gmail.com>
    Co-authored-by: Dognose <dognose24@users.noreply.github.com>
    Co-authored-by: Osk <oskosk@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3187800693

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f654b49a5fa6a3c8900b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '99b1bc527359ecd50c84');
