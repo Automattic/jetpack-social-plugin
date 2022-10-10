f903576da997816d81500a550817bb1d3499174d
retrofox <retrofox@users.noreply.github.com>
2022-10-10 11:05:06 +0000

    RNA: Fix style issue on Button component with icon and text (#26704)
    * fix styling issue with gutenberg button component update
    
    * changelog
    
    * bump version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3218744318

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2f52b8f0bf1298622123');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9ee81d2055658c351ab6');
