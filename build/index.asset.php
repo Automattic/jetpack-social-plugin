e8e1026831a54d3184c2a85d8f7c65bfc12fe01e
jsnmoon <jsnmoon@users.noreply.github.com>
2022-10-12 21:11:59 +0000

    Search: Add tooltips to show upgraded usage limits information (#26790)
    * Add shadowAnchor and forceShow for pure Popover
    
    * Introduce shadowed IconTooltips to DonutMeters
    
    * Introduce feature just_upgraded to enable tooltips
    
    * changelog
    
    * Bump versions
    
    * Add IconTooltipProps annotations
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3237809250

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '54ae4e2921c35ddd7c2d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd62d5871e6c0287b7840');
