3bbdf07bd06046559b31fd7f98f47dac338b4b40
oskosk <oskosk@users.noreply.github.com>
2022-10-21 10:46:59 +0000

    Connection: Add selectors to get user and blog data (#26978)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3296748303

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bb24dac1f27c9e8ffa14');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f5f07817074d5e9ec96c');
