6637bd0feecc4fc4f569acaff4e4b2cd52a13060
retrofox <retrofox@users.noreply.github.com>
2022-10-24 18:28:50 +0000

    Jetpack:  Fix visual issues in the Product Price component (#27032)
    * RNA: add literal CSS classes to apply styles in the regular way
    
    * Jetpack: fix styles in Product Price -> leyend
    
    * add a literal CSS class to Product price component
    
    * tweak product price subsection
    
    * changelog
    
    * bump new RNA version
    
    * Update projects/js-packages/components/components/product-price/index.tsx
    
    Co-authored-by: Douglas Henri <dhasilva@users.noreply.github.com>
    
    * Update projects/plugins/jetpack/_inc/client/components/jetpack-product-card/style.scss
    
    Co-authored-by: Douglas Henri <dhasilva@users.noreply.github.com>
    
    Co-authored-by: Douglas Henri <dhasilva@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3315109616

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f5f07817074d5e9ec96c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a69afdee5719c952ed33');
