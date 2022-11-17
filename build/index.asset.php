6ac2009e729c0712c6bbe89f0bb700c319b70f1b
nateweller <nateweller@users.noreply.github.com>
2022-11-17 13:40:26 +0000

    JS Components: Allow null offPrice values in ProductPrice (#27456)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3488675559

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '557d0e7437a033f6c531');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'afe12bba5fdf2dd5c427');
