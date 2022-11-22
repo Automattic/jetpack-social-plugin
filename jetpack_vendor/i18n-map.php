95d29843d862d837ee0a71584fb57d99b5789604
nunyvega <nunyvega@users.noreply.github.com>
2022-11-22 17:04:14 +0000

    Jetpack VideoPress: Prepare release 1.1.0-beta (#27554)
    * Prepare release 1.1.0-beta of Jetpack VideoPress standalone
    
    * Update projects/plugins/videopress/CHANGELOG.md
    
    include the -beta flag
    
    Co-authored-by: Steve D. <33553323+sdixon194@users.noreply.github.com>
    
    * Update projects/plugins/videopress/readme.txt
    
    include the -beta flag
    
    Co-authored-by: Steve D. <33553323+sdixon194@users.noreply.github.com>
    
    Co-authored-by: Steve D. <33553323+sdixon194@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3525364164

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,43 +6,43 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.13-alpha',
+      'ver' => '0.2.13',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.17.26-alpha',
+      'ver' => '1.17.26',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '1.11.1-alpha',
+      'ver' => '1.11.1',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.46.4-alpha',
+      'ver' => '1.46.4',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.8.32-alpha',
+      'ver' => '0.8.32',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.2.34-alpha',
+      'ver' => '2.2.34',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.7.12-alpha',
+      'ver' => '1.7.12',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.4.1-alpha',
+      'ver' => '2.4.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.2.8-alpha',
+      'ver' => '0.2.8',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.2.1-alpha',
+      'ver' => '0.2.1',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.43.1-alpha',
+      'ver' => '1.43.1',
     ),
   ),
 );
