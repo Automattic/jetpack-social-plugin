a97fd51de0e9de4b78ee617ade680c5c04cb41ce
miguelxpn <miguelxpn@users.noreply.github.com>
2022-11-30 18:47:58 +0000

    Protect 1.1.2: Changelog and Readme (#27683)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3586104970

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.47.0-alpha',
+      'ver' => '1.47.0',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.5.1-alpha',
+      'ver' => '2.5.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.44.0-alpha',
+      'ver' => '1.44.0',
     ),
   ),
 );
