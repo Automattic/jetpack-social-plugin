e76894e702a7cba76f1c5e6bc52becda547e88b5
anomiex <anomiex@users.noreply.github.com>
2023-03-15 15:46:33 +0000

    Update dependency automattic/wordbless to v0.4.2 (#29480)
    * Update dependency automattic/wordbless to v0.4.2
    * Remove `@before` etc on tests using `WorDBless\BaseTestCase`.
      The new version calls these itself, and the double calls break some tests.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4427955442

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,11 +18,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.51.1',
+      'ver' => '1.51.2-alpha',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.8.39',
+      'ver' => '0.8.40-alpha',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.2.11',
+      'ver' => '0.2.12-alpha',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.47.2',
+      'ver' => '1.47.3-alpha',
     ),
   ),
 );
