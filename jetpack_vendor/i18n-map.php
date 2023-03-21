022d86a1073adeb9f92707f9750f1c0f9e8a21e6
gogdzl <gogdzl@users.noreply.github.com>
2023-03-21 14:20:34 +0000

    CRM: UI Cohesiveness Make Naming Consistent (#29267)
    * CRM: Change from Customer ID to Contact ID in the Contact Edit page (#29195)
    
    * Change text from Customer to Contact
    
    * Kind of fix indentation
    
    * changelog
    
    * Make entire text translatable
    
    Co-authored-by: André Kallehauge <3846700+kallehauge@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: André Kallehauge <3846700+kallehauge@users.noreply.github.com>
    
    * CRM: Consistent naming for Tasks (#29240)
    
    * Rename from Event to Task
    
    * changelog
    
    * Fix misplaced comma
    
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>
    
    ---------
    
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>
    
    * Fix/crm UI cohesiveness invoicing pro referred to as csv pro (#29295)
    
    * Fix Invoicing Pro being referred to as CSV PRO, and lint everything (sorry for the mess)
    
    * changelog
    
    * CRM: Change customer references to contact (#29313)
    
    Co-authored-by: gogdzl <37049295+gogdzl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: André Kallehauge <3846700+kallehauge@users.noreply.github.com>
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4479876862

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.17.34-alpha',
+      'ver' => '1.17.34',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,15 +18,15 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.51.2-alpha',
+      'ver' => '1.51.2',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.8.40-alpha',
+      'ver' => '0.8.40',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.2-alpha',
+      'ver' => '2.3.2',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -34,11 +34,11 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.8.1-alpha',
+      'ver' => '2.8.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.2.12-alpha',
+      'ver' => '0.2.12',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -46,11 +46,11 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.22.0-alpha',
+      'ver' => '0.22.0',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.47.3-alpha',
+      'ver' => '1.47.3',
     ),
   ),
 );
