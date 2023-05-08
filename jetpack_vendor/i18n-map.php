34c4dc84a997844e5a6810e956a5d4776a10d195
anomiex <anomiex@users.noreply.github.com>
2023-05-08 16:55:31 +0000

    Backport changelogs from Jetpack 12.2-a.3 release (#30511)
    * Changelog edits.
    * Init Jetpack 12.2-a.4
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4917437603

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.8-alpha',
+      'ver' => '2.3.8',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.12.1-alpha',
+      'ver' => '2.12.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -50,11 +50,11 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.27.0-alpha',
+      'ver' => '0.27.0',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.48.0-alpha',
+      'ver' => '1.48.0',
     ),
   ),
 );
