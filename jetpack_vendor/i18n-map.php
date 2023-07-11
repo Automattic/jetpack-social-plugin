eb22af39b6d3629bb1fe12ffc9d110ab661fed28
thingalon <thingalon@users.noreply.github.com>
2023-07-11 11:18:46 +0000

    Kick off next dev cycle for boost (#31796)
    * Update changelogs and readmes
    
    * Prepare next cycle
    
    * Update composer.lock file
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    Co-authored-by: Adnan Haque <adnan007.id@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5519055549

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.7-alpha1689008943',
+      'ver' => '1.18.7',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.53.3-alpha1689008943',
+      'ver' => '1.53.3',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.12-alpha1689008943',
+      'ver' => '2.3.12',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '3.1.2-alpha1689008943',
+      'ver' => '3.1.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
