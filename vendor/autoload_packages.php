682c247ef462216ca128b822f7445678d49f0514
jeherve <jeherve@users.noreply.github.com>
2023-08-30 18:44:54 +0000

    General: update WordPress requirements to WP 6.2 (#32762)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6028415617

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ2_2_0;
+namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ2_2_1_alpha;
 
  // phpcs:ignore
 
