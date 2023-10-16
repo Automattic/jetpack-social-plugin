9ee244db75e192ab466db5e3799fa69c496b6b8e
ice9js <ice9js@users.noreply.github.com>
2023-10-16 19:06:09 +0000

    Backport chaanges for mu-wpcom-plugin 1.7.1 and jejtpack 12.8-a.1 (#33619)
    * Changelog edits.
    
    * Set version to 12.8-a.2
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6537945473

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.10.7-alpha1697047949',
+      'ver' => '0.10.7',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '3.8.2-alpha1697133599',
+      'ver' => '3.8.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -50,11 +50,11 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.36.3-alpha1697193230',
+      'ver' => '0.36.3',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.58.0-alpha1697195883',
+      'ver' => '1.58.0',
     ),
   ),
 );
