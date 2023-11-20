7135517bdfc13e09defea3aa5febfaafde5356ab
anomiex <anomiex@users.noreply.github.com>
2023-11-20 21:22:17 +0000

    Backport mu-wpcom-plugin 2.0.0, jetpack 12.9-a.7 Changes (#34213)
    * Add changelogs
    
    * Changelog edits.
    
    * Fix project versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6935801739

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,55 +6,55 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.3.0-alpha1700485793',
+      'ver' => '0.3.0',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.0.0-alpha1700493473',
+      'ver' => '2.0.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '2.0.0-alpha1700485793',
+      'ver' => '2.0.0',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.0.0-alpha1700493473',
+      'ver' => '2.0.0',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.12.0-alpha1700493473',
+      'ver' => '0.12.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.2.0-alpha1700485793',
+      'ver' => '0.2.0',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.0.0-alpha1700485793',
+      'ver' => '3.0.0',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '2.0.0-alpha1700485793',
+      'ver' => '2.0.0',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.0.0-alpha1700493473',
+      'ver' => '4.0.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.3.0-alpha1700485793',
+      'ver' => '0.3.0',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.3.0-alpha1700485793',
+      'ver' => '0.3.0',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.37.0-alpha1700493473',
+      'ver' => '0.37.0',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.0.0-alpha1700493473',
+      'ver' => '2.0.0',
     ),
   ),
 );
