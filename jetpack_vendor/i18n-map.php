e3a9215010ba1be00b389bdceca76799695e9865
anomiex <anomiex@users.noreply.github.com>
2024-03-26 16:43:00 +0000

    Update wordpress monorepo (#36585)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8439492334

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.5',
+      'ver' => '2.1.6-alpha1711470495',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.7.0-alpha1711453902',
+      'ver' => '2.7.0-alpha1711470495',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.17.5',
+      'ver' => '0.17.6-alpha1711470495',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.2',
+      'ver' => '3.1.3-alpha1711470495',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.19.0-alpha1711465143',
+      'ver' => '4.19.0-alpha1711470495',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.42.7',
+      'ver' => '0.42.8-alpha1711470495',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
