84eb5369959ef1f05c1111a922cb7199a6da2191
anomiex <anomiex@users.noreply.github.com>
2024-04-08 08:15:59 +0000

    Fix obviously wrong class name references (#36769)
    Almost all are phpdoc comments where the class was missing a `use` or
    leading `\`. More specifically, I searched for cases where Phan was
    complaining about classes named like `\Automattic\...\Automattic\`, or
    `\WP_...`, or `Exception` classes, and fixed all the obvious ones.
    
    Then I also searched for "undeclared {class|type} \[a-z]" and fixed the
    obvious ones out of that too (e.g. "obj", "number", and various typos).
    
    Note that having the correct typing allows Phan to complain about
    possibly-real issues in some cases. Those will have to be reviewed
    later.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8596601938

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.3.10-alpha1712170622',
+      'ver' => '0.3.10-alpha1712563319',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.7.1-alpha1712337314',
+      'ver' => '2.7.1-alpha1712563319',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.18.3-alpha1712337314',
+      'ver' => '0.18.3-alpha1712563319',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,15 +38,15 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.4-alpha1712337314',
+      'ver' => '3.1.4-alpha1712563319',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '2.0.3',
+      'ver' => '2.0.4-alpha1712563319',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.21.0-alpha1712340608',
+      'ver' => '4.21.0-alpha1712563319',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,19 +54,19 @@ return array(
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.3.3',
+      'ver' => '0.3.4-alpha1712563319',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.6.1',
+      'ver' => '0.6.2-alpha1712563319',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.42.9-alpha1712337314',
+      'ver' => '0.42.9-alpha1712563319',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.11.0-alpha1712134333',
+      'ver' => '2.11.0-alpha1712563319',
     ),
   ),
 );
