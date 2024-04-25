d85eb54a31043dcd8806932f840c833c6b4bd377
anomiex <anomiex@users.noreply.github.com>
2024-04-25 17:57:16 +0000

    phan: Fix more phpdocs (#37042)
    Another pass through the PhanUndeclared issues looking for issues that
    are due to phpdocs being wrong.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8837036730
    
    Upstream-Ref: Automattic/jetpack@f31bc10281f0e855e6854cecb6d80ede21dac64a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.7.4-alpha1714062343',
+      'ver' => '2.7.4-alpha1714066953',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.18.4',
+      'ver' => '0.18.5-alpha1714066953',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.22.2',
+      'ver' => '4.22.3-alpha1714066953',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -62,11 +62,11 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.42.12',
+      'ver' => '0.42.13-alpha1714066953',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.13.1',
+      'ver' => '2.13.2-alpha1714066953',
     ),
   ),
 );
