a07d880191b34f18af717316a07e5330b6affe8c
sergeymitr <sergeymitr@users.noreply.github.com>
2024-05-28 15:53:59 +0000

    Connection: hoist Identity Crisis package back into Connection (#36968)
    * Move the Identity Crisis package contents into Connection package.
    * Abandon the Identity Crisis package.
    * Ensure backward and forward compatibility for existing code.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9272329232
    
    Upstream-Ref: Automattic/jetpack@ce733fc8231eefeca82564a92fb2d722551d39c8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,11 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.8.6',
-    ),
-    'jetpack-idc' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.20.0',
+      'ver' => '2.9.0-alpha1716910709',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -66,7 +62,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.0.0',
+      'ver' => '3.0.1-alpha1716910709',
     ),
   ),
 );
