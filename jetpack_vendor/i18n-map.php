dfac895486584b903e2a7570e10f660f54494ace
anomiex <anomiex@users.noreply.github.com>
2024-08-21 21:04:45 +0000

    Update dependency yoast/phpunit-polyfills to ^1.1.1 (#39004)
    * Update dependency yoast/phpunit-polyfills to 1.1.0 || 1.1.1
    * Caret version
    * wpcomsh: Remove unnecessary phpunit dep.
      `yoast/phpunit-polyfills` already brings in an appropriate version of
      phpunit.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10497266284
    
    Upstream-Ref: Automattic/jetpack@480d5da856bf545238871951e102ca8a200b5495

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.4.2',
+      'ver' => '0.4.3-alpha1724273335',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.3',
+      'ver' => '2.3.4-alpha1724273335',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.2.8',
+      'ver' => '0.2.9-alpha1724273335',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.3.11',
+      'ver' => '0.3.12-alpha1724273335',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,55 +26,55 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.12.3',
+      'ver' => '2.12.4-alpha1724273335',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.1.3',
+      'ver' => '0.1.4-alpha1724273335',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.2.2',
+      'ver' => '0.2.3-alpha1724273335',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.17',
+      'ver' => '3.1.18-alpha1724273335',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '2.0.6',
+      'ver' => '2.0.7-alpha1724273335',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.33.0-alpha1724263615',
+      'ver' => '4.33.0-alpha1724273335',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.3.1',
+      'ver' => '0.3.2-alpha1724273335',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.4.1',
+      'ver' => '0.4.2-alpha1724273335',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.6.2',
+      'ver' => '0.6.3-alpha1724273335',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.2.0',
+      'ver' => '0.2.1-alpha1724273335',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.1.2',
+      'ver' => '0.1.3-alpha1724273335',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.49.3-alpha1724263615',
+      'ver' => '0.49.3-alpha1724273335',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.7.0',
+      'ver' => '3.7.1-alpha1724273335',
     ),
   ),
 );
--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,15 +7,15 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '2.3.3.0',
+		'version' => '2.3.4.0-alpha1724273335',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd9927a8ddcd8b3a40fb28c24213827ff' => array(
-		'version' => '0.49.3.0-alpha1724263615',
+		'version' => '0.49.3.0-alpha1724273335',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-publicize/actions.php'
 	),
 	'e6f7f640a6586216432b53e5c9d1b472' => array(
-		'version' => '0.49.3.0-alpha1724263615',
+		'version' => '0.49.3.0-alpha1724273335',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-publicize/src/social-image-generator/utilities.php'
 	),
 	'3d6facae188ac62d1f8a943582feb57b' => array(
