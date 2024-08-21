52f4b6de3a4bef04712a7c009e83632b7d6d4652
anomiex <anomiex@users.noreply.github.com>
2024-08-21 18:21:35 +0000

    Backport mu-wpcom-plugin 2.5.8, jetpack 13.8-a.5, wpcomsh 5.5.0 Changes (#38993)
    * Changelog and readme.txt edits.
    * Version bumps
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10495096273
    
    Upstream-Ref: Automattic/jetpack@828a8f2e8599e0f23e4ebb9287aa1293939ea9a0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.3-alpha1724171952',
+      'ver' => '2.3.3',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.12.3-alpha1724252288',
+      'ver' => '2.12.3',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.1.3-alpha1724171952',
+      'ver' => '0.1.3',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.33.0-alpha1724261266',
+      'ver' => '4.33.0-alpha1724263615',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,11 +70,11 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.49.2-alpha1724258303',
+      'ver' => '0.49.3-alpha1724263615',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.7.0-alpha1724141518',
+      'ver' => '3.7.0',
     ),
   ),
 );
