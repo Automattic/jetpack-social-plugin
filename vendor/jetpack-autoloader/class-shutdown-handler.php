ce820c8e02920b21be1a9829bbfd601d8846ca8c
gmjuhasz <gmjuhasz@users.noreply.github.com>
2024-10-02 14:13:37 +0000

    Updated package versions for automattic/jetpack-social
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11145449463
    
    Upstream-Ref: Automattic/jetpack@26bcd34939539fa42dcf91ddcabdd9017d0bc01e

--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ5_4_0\al3_1_0;
+namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ5_4_1\al3_1_0;
 
  // phpcs:ignore
 
