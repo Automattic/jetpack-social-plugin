94827c337d88860ebbefe68c0959e70e85d7a46a
ice9js <ice9js@users.noreply.github.com>
2024-10-14 13:57:16 +0000

    Backport jetpack 14.0-a.3 Changes (#39760)
    * Changelog and readme.txt edits.
    
    * Add changelog entry for Jetpack 13.9.1
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11328814297
    
    Upstream-Ref: Automattic/jetpack@15f3015e3b3c3d3d5886457ee6d0067860bcae8c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.1.12-alpha1728575216',
+      'ver' => '0.1.12',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.26-alpha1728575216',
+      'ver' => '3.1.26',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.35.13-alpha1728575216',
+      'ver' => '4.35.13',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,11 +70,11 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.54.3-alpha1728505205',
+      'ver' => '0.54.3',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.14.1-alpha1728893254',
+      'ver' => '3.14.2-alpha1728913359',
     ),
   ),
 );
