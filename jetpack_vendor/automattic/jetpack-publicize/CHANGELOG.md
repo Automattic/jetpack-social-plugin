18eef025904d02992d010ecbf5a12d2f6d044d13
anomiex <anomiex@users.noreply.github.com>
2024-11-21 16:23:25 +0000

    Update wordpress monorepo (#40288)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11957107408
    
    Upstream-Ref: Automattic/jetpack@88204ab00dc607b193511fea05c6d116e25c4d9b

--- jetpack_vendor/automattic/jetpack-publicize/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-publicize/CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.56.1-alpha] - unreleased
+
+This is an alpha version! The changes listed here are not final.
+
+### Changed
+- Updated package dependencies.
+
 ## [0.56.0] - 2024-11-18
 ### Added
 - Added a new toggle for UTM tracking [#39998]
@@ -759,6 +766,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Updated package dependencies.
 - Update package.json metadata.
 
+[0.56.1-alpha]: https://github.com/Automattic/jetpack-publicize/compare/v0.56.0...v0.56.1-alpha
 [0.56.0]: https://github.com/Automattic/jetpack-publicize/compare/v0.55.2...v0.56.0
 [0.55.2]: https://github.com/Automattic/jetpack-publicize/compare/v0.55.1...v0.55.2
 [0.55.1]: https://github.com/Automattic/jetpack-publicize/compare/v0.55.0...v0.55.1
--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.0-alpha1732046180',
+      'ver' => '4.0.0-alpha1732205217',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.1.0-alpha1732046180',
+      'ver' => '6.1.0-alpha1732205217',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.1-alpha1731956635',
+      'ver' => '0.2.1-alpha1732205217',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.0.1-alpha1732046180',
+      'ver' => '4.0.1-alpha1732205217',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.0.2-alpha1732112016',
+      'ver' => '5.0.2-alpha1732205217',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.56.0',
+      'ver' => '0.56.1-alpha1732205217',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
