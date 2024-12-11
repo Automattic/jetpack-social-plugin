a131e093682fccc38ffec67a436cfc6d05715304
anomiex <anomiex@users.noreply.github.com>
2024-12-11 16:07:20 +0000

    Update to eslint 9 (#40555)
    Eslint 9 brings a new config format, which means this PR also rewrites
    all the eslint configs. I took the opportunity to clean them up
    (although a good bit of that wound up in earlier PRs), and added code to
    fetch the textdomain from composer.json and whether the project uses
    React from package.json so we don't need to have so many boilerplate
    configs that just set a textdomain or enable React rules.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12280005125
    
    Upstream-Ref: Automattic/jetpack@9e3ae2f2e3140f60ae88df608bc1cbc34d275109

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.1',
+      'ver' => '4.0.2-alpha1733932294',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.2.0',
+      'ver' => '6.2.1-alpha1733932294',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.0.2',
+      'ver' => '4.0.3-alpha1733932294',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.1.2-alpha1733760228',
+      'ver' => '5.1.2-alpha1733932294',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.7.1',
+      'ver' => '0.7.2-alpha1733932294',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.56.4',
+      'ver' => '0.56.5-alpha1733932294',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
