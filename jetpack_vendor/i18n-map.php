8ae09725152ddbdf434b1b21df14781daed95aec
gcsecsey <gcsecsey@users.noreply.github.com>
2025-02-11 17:50:40 +0000

    Update query parameter for JITM requests (#41542)
    * Update query parameter for JITM requests
    
    * changelog
    
    * Use null coalescing operator
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13268950573
    
    Upstream-Ref: Automattic/jetpack@821e82b01d1c4c97e9e3da198f31a25c4e8428c0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.5',
+      'ver' => '4.0.6',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.4.0',
+      'ver' => '6.4.1',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.0.6',
+      'ver' => '4.0.7-alpha1739295229',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.4.3-alpha1739291673',
+      'ver' => '5.4.4-alpha1739293591',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,11 +66,11 @@ return array(
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.1-alpha1739288537',
+      'ver' => '0.5.1',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.60.0-alpha1739288537',
+      'ver' => '0.60.0',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
