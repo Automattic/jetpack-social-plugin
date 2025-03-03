680a2688394515f51bb083926c5962b9441430c7
zinigor <zinigor@users.noreply.github.com>
2025-03-03 16:47:22 +0000

    Update wordpress monorepo (#42163)
    * Update wordpress monorepo
    
    * Added an enhanced mock to the test, h/t @anomiex for the fix!
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Igor Zinovyev <zinigor@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13635659867
    
    Upstream-Ref: Automattic/jetpack@e65723f397e646f0c9807d4751d1d8625e037968

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.8',
+      'ver' => '4.0.9-alpha1741019398',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.0-alpha1740754670',
+      'ver' => '6.7.0-alpha1741019398',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.8',
+      'ver' => '0.2.9-alpha1741019398',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.0-alpha1740577331',
+      'ver' => '4.2.0-alpha1741019398',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.5.1-alpha1740763134',
+      'ver' => '5.5.1-alpha1741019398',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.3',
+      'ver' => '0.8.4-alpha1741019398',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.61.1-alpha1740998893',
+      'ver' => '0.61.1-alpha1741019398',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
