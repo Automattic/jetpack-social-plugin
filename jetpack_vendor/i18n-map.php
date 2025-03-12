e3d5392e2eba98642e4f6962b7ef72d2cb23bf58
anomiex <anomiex@users.noreply.github.com>
2025-03-12 14:44:43 +0000

    phpunit: Use phpunit-select-config, and some cleanup (#42266)
    * Remove obsolete polyfills
    * gitignore `.phpunit.cache` by default
    * production-exclude `phpunit.xml.dist` and `phpunit.*.xml.dist` by default
    * production-exclude tests dirs
    * Use phpunit-select-config everywhere
    * cli: Support copying symlinks for `jetpack generate`
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13813825579
    
    Upstream-Ref: Automattic/jetpack@237f3f58cab74f799f6178c610f757e80343d7ac

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.4',
+      'ver' => '0.5.5-alpha1741789611',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.10',
+      'ver' => '4.0.11-alpha1741789611',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.3.7',
+      'ver' => '0.3.8-alpha1741789611',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.2',
+      'ver' => '0.4.3-alpha1741789611',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,55 +26,55 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.4-alpha1741785051',
+      'ver' => '6.7.4-alpha1741789611',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.10',
+      'ver' => '0.2.11-alpha1741789611',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.3',
+      'ver' => '0.4.4-alpha1741789611',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.1',
+      'ver' => '4.2.2-alpha1741789611',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.0.6',
+      'ver' => '3.0.7-alpha1741789611',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.5.4-alpha1741723608',
+      'ver' => '5.5.4-alpha1741789611',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.4',
+      'ver' => '0.4.5-alpha1741789611',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.2',
+      'ver' => '0.5.3-alpha1741789611',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.5',
+      'ver' => '0.8.6-alpha1741789611',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.5.2',
+      'ver' => '0.5.3-alpha1741789611',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.4',
+      'ver' => '0.5.5-alpha1741789611',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.61.3-alpha1741785051',
+      'ver' => '0.61.3-alpha1741789611',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.8.3',
+      'ver' => '4.8.4-alpha1741789611',
     ),
   ),
 );
