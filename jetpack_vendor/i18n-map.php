a1725adae1241e87c36563769a7f74044f5b61fc
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-03-21 09:27:29 +0000

    Updated package versions for automattic/jetpack-social
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13988567696
    
    Upstream-Ref: Automattic/jetpack@55616479d2a840fa5d9576fd6164fb13573e1b43

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.7-alpha1742415897',
+      'ver' => '0.5.7',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.14-alpha1742417137',
+      'ver' => '4.0.14',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.3.9-alpha1742415897',
+      'ver' => '0.3.9',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.6-alpha1742415897',
+      'ver' => '0.4.6',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,55 +26,55 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.7-alpha1742417137',
+      'ver' => '6.7.7',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.13-alpha1742415897',
+      'ver' => '0.2.13',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.6-alpha1742415897',
+      'ver' => '0.4.6',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.6-alpha1742415897',
+      'ver' => '4.2.6',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.0.8-alpha1742415897',
+      'ver' => '3.0.8',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.8.0-alpha1742493741',
+      'ver' => '5.8.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.7-alpha1742415897',
+      'ver' => '0.4.7',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.4-alpha1742415897',
+      'ver' => '0.5.4',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.10-alpha1742415897',
+      'ver' => '0.8.10',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.5.4-alpha1742415897',
+      'ver' => '0.5.4',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.8-alpha1742415897',
+      'ver' => '0.5.8',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.62.2-alpha1742537507',
+      'ver' => '0.62.2',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.9.2-alpha1742545545',
+      'ver' => '4.9.2',
     ),
   ),
 );
--- vendor/jetpack-autoloader/class-version-loader.php
+++ vendor/jetpack-autoloader/class-version-loader.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ6_3_0_alpha\al5_0_5_alpha;
+namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ6_3_0\al5_0_5;
 
  // phpcs:ignore
 
