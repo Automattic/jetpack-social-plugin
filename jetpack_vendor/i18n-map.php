6a8af7516a3f12c3a754deb31bb9354b6c09d33f
anomiex <anomiex@users.noreply.github.com>
2025-04-22 20:24:06 +0000

    phpunit: Enable reporting and failing on PHPUnit 11 deprecations (#43179)
    Now that we've fixed all the PHPUnit 11 deprecations, adjust the configs
    to enable `displayDetailsOnPhpunitDeprecations` and
    `failOnPhpunitDeprecation`.
    
    For plugins/crm, plugins/jetpack, and plugins/wpcomsh, there's one more
    deprecation left that's also fixed in this PR: WP Core's obsolete
    `WP_UnitTestCase_Base::checkRequirements()` function has a doc comment
    that PHPUnit is mis-parsing to see a `@group` annotation that's not
    actually intended. We can override it to stop PHPUnit from seeing that
    doc comment.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14603629889
    
    Upstream-Ref: Automattic/jetpack@c78f78dae7d54c7cd98b5b189bd4c4485fb100c5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.8-alpha1745351100',
+      'ver' => '0.5.8-alpha1745352378',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.19-alpha1745351100',
+      'ver' => '4.0.19-alpha1745352378',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.8-alpha1745351100',
+      'ver' => '0.4.8-alpha1745352378',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.1-alpha1745351100',
+      'ver' => '6.11.1-alpha1745352378',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -34,23 +34,23 @@ return array(
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.9-alpha1745351100',
+      'ver' => '0.4.9-alpha1745352378',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.14-alpha1745351100',
+      'ver' => '4.2.14-alpha1745352378',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.0.8',
+      'ver' => '3.0.9-alpha1745352378',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.12.1-alpha1745351100',
+      'ver' => '5.12.1-alpha1745352378',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.8-alpha1745351100',
+      'ver' => '0.4.8-alpha1745352378',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -58,23 +58,23 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.13-alpha1745351100',
+      'ver' => '0.8.13-alpha1745352378',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.5.4',
+      'ver' => '0.5.5-alpha1745352378',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.10-alpha1745351100',
+      'ver' => '0.5.10-alpha1745352378',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.64.2-alpha1745351100',
+      'ver' => '0.64.2-alpha1745352378',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.11.1-alpha1745351100',
+      'ver' => '4.11.1-alpha1745352378',
     ),
   ),
 );
