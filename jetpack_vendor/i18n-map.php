69f252d14bd9f912ff4db83b101df5c95f6c00c9
anomiex <anomiex@users.noreply.github.com>
2025-04-24 16:58:39 +0000

    phpunit: Update to yoast/phpunit-polyfills v4 (#43231)
    This brings in use of PHPUnit 12 for PHP 8.3+. Other than needing
    `phpunit.12.xml.dist` files (as symlinks to the PHPUnit 11 configs),
    this seems to work without issue after a few cleanups in #43215
    and #43217.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14646946984
    
    Upstream-Ref: Automattic/jetpack@320d92e7a0e6be41533b5f88f6b2fd0cdc13f163

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.8-alpha1745352378',
+      'ver' => '0.5.8-alpha1745512883',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.19-alpha1745352378',
+      'ver' => '4.0.19-alpha1745512883',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.3.9',
+      'ver' => '0.3.10-alpha1745512883',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.8-alpha1745352378',
+      'ver' => '0.4.8-alpha1745512883',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,55 +26,55 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.1-alpha1745436992',
+      'ver' => '6.11.1-alpha1745512883',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.16',
+      'ver' => '0.2.17-alpha1745512883',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.9-alpha1745352378',
+      'ver' => '0.4.9-alpha1745512883',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.14-alpha1745436992',
+      'ver' => '4.2.14-alpha1745512883',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.0.9-alpha1745352378',
+      'ver' => '3.0.9-alpha1745512883',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.13.0-alpha1745478261',
+      'ver' => '5.13.0-alpha1745512883',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.8-alpha1745352378',
+      'ver' => '0.4.8-alpha1745512883',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.4',
+      'ver' => '0.5.5-alpha1745512883',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.13-alpha1745352378',
+      'ver' => '0.8.13-alpha1745512883',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.5.5-alpha1745352378',
+      'ver' => '0.5.5-alpha1745512883',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.10-alpha1745352378',
+      'ver' => '0.5.10-alpha1745512883',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.64.2-alpha1745352378',
+      'ver' => '0.64.2-alpha1745512883',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.11.1-alpha1745486636',
+      'ver' => '4.11.1-alpha1745512883',
     ),
   ),
 );
