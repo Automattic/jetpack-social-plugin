a9d88b56a3dd2efa145acdb5962d7a4f8f021ffc
ivan-ottinger <ivan-ottinger@users.noreply.github.com>
2025-05-15 08:47:21 +0000

    Sync: Add support for `sandbox` environment type (#43430)
    * Add support for sandbox environment type in wp_get_environment_type
    
    * Add changelog file
    
    * Update wp_get_environment_type to use Functions::get_environment_type
    
    * Add tests related to get_environment_type()
    
    * Return early
    
    * Add getenv support and related test
    
    * Remove unnecessary returns
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15040362227
    
    Upstream-Ref: Automattic/jetpack@293b32d077def40ea1686772bfc81585e4dacb51

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -74,7 +74,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.13.0-alpha1747128530',
+      'ver' => '4.13.0-alpha1747297904',
     ),
   ),
 );
