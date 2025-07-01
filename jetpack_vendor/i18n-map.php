7ececf3ada60b90da036ce4119cc7c8bc33e8720
anomiex <anomiex@users.noreply.github.com>
2025-07-01 15:03:02 +0000

    Update wordpress monorepo (#44148)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16002658658
    
    Upstream-Ref: Automattic/jetpack@fe0b5c1f2ba828f96569bc014583db89d3216b84

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.31',
+      'ver' => '4.0.32-alpha1751381215',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.13-alpha1751380581',
+      'ver' => '6.13.13-alpha1751381215',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.3.2-alpha1751378721',
+      'ver' => '0.3.2-alpha1751381215',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.26',
+      'ver' => '4.2.27-alpha1751381215',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.17.2-alpha1751380581',
+      'ver' => '5.17.2-alpha1751381215',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.20',
+      'ver' => '0.8.21-alpha1751381215',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.65.8-alpha1751380581',
+      'ver' => '0.65.8-alpha1751381215',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
