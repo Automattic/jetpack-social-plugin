19fe7e83cabbe1bc8da39804cef725a73c40260e
tbradsha <tbradsha@users.noreply.github.com>
2025-07-23 13:51:13 +0000

    Stylelint: Address remaining stylistic rules (#44403)
    * Fix @stylistic/declaration-block-trailing-semicolon
    
    * Fix @stylistic/no-extra-semicolons
    
    * Fix @stylistic/no-eol-whitespace
    
    * Fix @stylistic/number-leading-zero and @stylistic/number-no-trailing-zeros
    
    * Fix @stylistic/color-hex-case
    
    * Fix @stylistic/property-case
    
    * Fix @stylistic/string-quotes
    
    * Fix @stylistic/max-line-length
    
    * Add changelogs
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16472168446
    
    Upstream-Ref: Automattic/jetpack@337c2ec36fff6fe4e3df7a66e9a592f79343cf54

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.0-alpha1753180992',
+      'ver' => '4.3.0-alpha1753277640',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.16.0-alpha1753260548',
+      'ver' => '6.16.0-alpha1753277640',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.0-alpha1753277546',
+      'ver' => '4.3.0-alpha1753277640',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.20.0-alpha1753273842',
+      'ver' => '5.20.0-alpha1753277640',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.23',
+      'ver' => '0.8.24-alpha1753277640',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
