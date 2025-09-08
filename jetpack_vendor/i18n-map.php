61f73aeb402596daa9fcbd99041a31154d25ccc1
tbradsha <tbradsha@users.noreply.github.com>
2025-09-08 17:15:23 +0000

    Prevent PHP fatals when directly accessing files, round 6 (#45105)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17558277447
    
    Upstream-Ref: Automattic/jetpack@b016fb729603257cf64cda327e600f0463870f64

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.6-alpha1757051536',
+      'ver' => '6.18.6-alpha1757350743',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.26.0-alpha1757303959',
+      'ver' => '5.26.0-alpha1757350743',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.66.11-alpha1757051536',
+      'ver' => '0.66.11-alpha1757350743',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
