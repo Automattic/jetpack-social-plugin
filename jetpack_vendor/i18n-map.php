5963299af9fc204b070c3e0f170f1d1fb23639b4
dilirity <dilirity@users.noreply.github.com>
2026-01-08 10:43:36 +0000

    Backport boost 4.5.5-beta changes (#46474)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20813623657
    
    Upstream-Ref: Automattic/jetpack@90810b4e9fd89dfaab6d6dec50e4897eebd6bbfc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.19-alpha1767721690',
+      'ver' => '4.3.19',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.20.0-alpha1767721690',
+      'ver' => '6.20.0',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.6-alpha1767739949',
+      'ver' => '0.4.6',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.14-alpha1767721690',
+      'ver' => '4.3.14',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.29.2-alpha1767739949',
+      'ver' => '5.29.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
