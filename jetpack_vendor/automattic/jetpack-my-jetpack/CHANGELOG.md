ea0d3e9efcfa985df99f4641dfa69ba4215b84ba
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-02-18 12:40:27 +0000

    Updated package versions for automattic/jetpack-social
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22139979692
    
    Upstream-Ref: Automattic/jetpack@791495cdd103afb17f93667747cec30fd4f074d9

--- jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
@@ -5,12 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [5.31.4-alpha] - unreleased
-
-This is an alpha version! The changes listed here are not final.
-
+## [5.31.4] - 2026-02-18
 ### Changed
-- Update package dependencies.
+- Update package dependencies. [#47173]
 
 ## [5.31.3] - 2026-02-16
 ### Changed
@@ -2538,7 +2535,7 @@ This is an alpha version! The changes listed here are not final.
 ### Added
 - Created package
 
-[5.31.4-alpha]: https://github.com/Automattic/jetpack-my-jetpack/compare/5.31.3...5.31.4-alpha
+[5.31.4]: https://github.com/Automattic/jetpack-my-jetpack/compare/5.31.3...5.31.4
 [5.31.3]: https://github.com/Automattic/jetpack-my-jetpack/compare/5.31.2...5.31.3
 [5.31.2]: https://github.com/Automattic/jetpack-my-jetpack/compare/5.31.1...5.31.2
 [5.31.1]: https://github.com/Automattic/jetpack-my-jetpack/compare/5.31.0...5.31.1
--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.25-alpha1771364022',
+      'ver' => '4.3.25',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '7.1.1-alpha1771364022',
+      'ver' => '7.1.1',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.12-alpha1771364022',
+      'ver' => '0.4.12',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.22-alpha1771364022',
+      'ver' => '4.3.22',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.31.4-alpha1771364022',
+      'ver' => '5.31.4',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.9.7-alpha1771364022',
+      'ver' => '0.9.7',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.72.4-alpha1771414840',
+      'ver' => '0.72.4',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
