bf1f29a1541c819d8de350268deaaab49518d45f
tbradsha <tbradsha@users.noreply.github.com>
2026-05-13 17:02:08 +0000

    Backport crm 6.8.0 changes (#48778)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25813102139
    
    Upstream-Ref: Automattic/jetpack@2549a500e8de37d3f03ce643e096d01c533e7e4d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.3',
+      'ver' => '0.8.4-alpha1778690619',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.35',
+      'ver' => '4.3.36',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.4',
+      'ver' => '8.3.5-alpha1778690619',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.23',
+      'ver' => '0.4.24-alpha1778690619',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.33',
+      'ver' => '4.3.34-alpha1778690619',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.9.17',
+      'ver' => '0.9.18-alpha1778690619',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
