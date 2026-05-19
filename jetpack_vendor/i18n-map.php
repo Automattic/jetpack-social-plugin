2a11b8b2f59294eaf17734abe0bbef6b4caaf85d
tbradsha <tbradsha@users.noreply.github.com>
2026-05-19 08:38:53 +0000

    Phan: Address PhanPluginDuplicateConditionalNullCoalescing violations (#48887)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26085253845
    
    Upstream-Ref: Automattic/jetpack@2a848e21f72597df8312885f7f66e25849942e00

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.6-alpha1778793320',
+      'ver' => '8.3.6-alpha1779178862',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.7.26-alpha1778793320',
+      'ver' => '0.7.26-alpha1779178862',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.37.2-alpha1779119236',
+      'ver' => '5.37.2-alpha1779178862',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,15 +78,15 @@ return array(
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.7.10-alpha1778793320',
+      'ver' => '0.7.10-alpha1779178862',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.81.0-alpha1779119236',
+      'ver' => '0.81.0-alpha1779178862',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.38.2-alpha1778793320',
+      'ver' => '4.38.2-alpha1779178862',
     ),
   ),
 );
