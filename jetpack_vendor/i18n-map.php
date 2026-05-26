cd9df89227e257d29ee402eefafec03b2090e684
CGastrell <CGastrell@users.noreply.github.com>
2026-05-26 18:37:30 +0000

    Update dependency @wordpress/ui to v0.13.0 (#49153)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26466885994
    
    Upstream-Ref: Automattic/jetpack@78b437461766a5e661fec0317346e0202f157274

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -82,7 +82,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.81.2-alpha1779752616',
+      'ver' => '0.81.2-alpha1779819649',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
