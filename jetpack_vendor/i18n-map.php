f3fa1ee0f89ba2d200a94efb58702ab7c47eb413
tbradsha <tbradsha@users.noreply.github.com>
2026-06-02 20:10:28 +0000

    Tests: Split code coverage scripts into test-js-coverage and test-php-coverage (#48845)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26844277266
    
    Upstream-Ref: Automattic/jetpack@b25e59181090ffcd52db1200e882dbb26ebc543e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,23 +6,23 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.8',
+      'ver' => '0.8.9-alpha1780429966',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.39',
+      'ver' => '4.3.40-alpha1780429966',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
-      'ver' => '0.3.7',
+      'ver' => '0.3.8-alpha1780429966',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.4.5',
+      'ver' => '0.4.6-alpha1780429966',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.15',
+      'ver' => '0.4.16-alpha1780429966',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -30,67 +30,67 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.2',
+      'ver' => '8.5.3-alpha1780429966',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.27-alpha1780428861',
+      'ver' => '0.4.27-alpha1780429966',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.7.27',
+      'ver' => '0.7.28-alpha1780429966',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.12',
+      'ver' => '0.4.13-alpha1780429966',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.38-alpha1780428861',
+      'ver' => '4.3.38-alpha1780429966',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.1.7',
+      'ver' => '3.1.8-alpha1780429966',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.37.7-alpha1780420216',
+      'ver' => '5.37.7-alpha1780429966',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.11',
+      'ver' => '0.4.12-alpha1780429966',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.9',
+      'ver' => '0.5.10-alpha1780429966',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.9.20-alpha1780428861',
+      'ver' => '0.9.20-alpha1780429966',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
-      'ver' => '0.1.4',
+      'ver' => '0.1.5-alpha1780429966',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.6.3',
+      'ver' => '0.6.4-alpha1780429966',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.7.11',
+      'ver' => '0.7.12-alpha1780429966',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.81.3-alpha1780383559',
+      'ver' => '0.81.3-alpha1780429966',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.39.0',
+      'ver' => '4.39.1-alpha1780429966',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.1.12-alpha1780428861',
+      'ver' => '0.1.12-alpha1780429966',
     ),
   ),
 );
--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ9_0_0\al5_0_18;
+namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_socialⓥ9_0_1_alpha\al5_0_19_alpha;
 
  // phpcs:ignore
 
