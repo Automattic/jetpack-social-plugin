79069b6d045514f254c72cc308130404579590dd
tbradsha <tbradsha@users.noreply.github.com>
2026-06-05 15:19:33 +0000

    Tooling: Remove legacy `test-coverage` keys (#49418)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27022636293
    
    Upstream-Ref: Automattic/jetpack@1bdbffc0148a0afdeaf00e832ff7681e9759bab5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,27 +6,27 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.0',
+      'ver' => '0.9.1-alpha1780671753',
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.3.0',
+      'ver' => '0.3.1-alpha1780671753',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.40',
+      'ver' => '4.3.41-alpha1780671753',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
-      'ver' => '0.3.8-alpha1780429966',
+      'ver' => '0.3.8-alpha1780671753',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.4.6',
+      'ver' => '0.4.7-alpha1780671753',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.16',
+      'ver' => '0.4.17-alpha1780671753',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -34,67 +34,67 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.4',
+      'ver' => '8.5.5-alpha1780671753',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.27',
+      'ver' => '0.4.28-alpha1780671753',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.7.28-alpha1780429966',
+      'ver' => '0.7.28-alpha1780671753',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.13',
+      'ver' => '0.4.14-alpha1780671753',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.38',
+      'ver' => '4.3.39-alpha1780671753',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.1.8',
+      'ver' => '3.1.9-alpha1780671753',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.38.0',
+      'ver' => '5.38.1-alpha1780671753',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.12',
+      'ver' => '0.4.13-alpha1780671753',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.10',
+      'ver' => '0.5.11-alpha1780671753',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.9.20-alpha1780429966',
+      'ver' => '0.9.20-alpha1780671753',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
-      'ver' => '0.1.5-alpha1780429966',
+      'ver' => '0.1.5-alpha1780671753',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.6.4',
+      'ver' => '0.6.5-alpha1780671753',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.7.12',
+      'ver' => '0.7.13-alpha1780671753',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.81.3-alpha1780578360',
+      'ver' => '0.81.3-alpha1780671753',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.39.1',
+      'ver' => '4.39.2-alpha1780671753',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.1.12-alpha1780429966',
+      'ver' => '0.1.12-alpha1780671753',
     ),
   ),
 );
