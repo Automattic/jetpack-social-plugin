44edd947718818827c009a9af23ac0a40db03a6b
simison <simison@users.noreply.github.com>
2026-07-13 15:52:13 +0000

    Publicize: remove calypso color schemes (#50452)
    * Remove Calypso color schemes and postcss-custom-properties PostCSS plugin in Publicize
    * Update one token to WPDS in Social previews package
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29262944960
    
    Upstream-Ref: Automattic/jetpack@5200d91a7f01bc1bf35eec4d0a9eaceadd86123f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.84.0-alpha1783936852',
+      'ver' => '0.84.0-alpha1783956979',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
