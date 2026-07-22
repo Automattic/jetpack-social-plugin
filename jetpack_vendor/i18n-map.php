48790681d12357fb14c2747e8d091a3a87308872
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-07-22 10:32:14 +0000

    Social: Add admin UI v2 feature flag (#50733)
    * Social: add admin UI v2 feature flag
    
    * Address feature flag review feedback
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29911180685
    
    Upstream-Ref: Automattic/jetpack@50fe4c5f1bbd9f9fc9093efc9d6ddca6001840de

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.84.2-alpha1784692289',
+      'ver' => '0.84.2-alpha1784715295',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
