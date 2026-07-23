16be3b4b1a5008cebcb69dbf83284b9fbc28a30d
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-07-23 10:53:57 +0000

    Social: add a connection-flow state machine to the social store (#50772)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/30000076625
    
    Upstream-Ref: Automattic/jetpack@5e87064f5fce8eed5d1e7bbbfb5d5b9e5d90608c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.84.2-alpha1784798846',
+      'ver' => '0.84.2-alpha1784803112',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
