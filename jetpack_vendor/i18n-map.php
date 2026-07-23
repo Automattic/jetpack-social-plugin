4af527d93a4f5e74d367ee683bd463a2586d7075
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-07-23 10:34:38 +0000

    My Jetpack: skip the connection onboarding flow on WordPress.com Simple sites (#50693)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29998727863
    
    Upstream-Ref: Automattic/jetpack@46cce5a7820c0ac8a6128a50576aed0b55c4e93e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.40.10-alpha1784740091',
+      'ver' => '5.40.10-alpha1784801834',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
