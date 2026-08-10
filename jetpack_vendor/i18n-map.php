89d9c9ec640c118da8cba2ef35b929ff1c494857
haqadn <haqadn@users.noreply.github.com>
2026-08-10 21:11:43 +0000

    Backport 16.1 release branch changes to trunk (#51150)
    * Improve validation and general code quality in bundled packages
    
    * Improve validation and general code quality in the Jetpack plugin
    
    * Prepare package releases
    
    * Adjust changelog wording
    
    * Prepare package releases
    
    * Adjust changelog wording
    
    * Adjust changelog wording
    
    (cherry picked from commit 09ad75ff61e6a4c1efe111f78a5d36b73a056558)
    
    * Resolve Redirect tests: allow the documentation-range fixture hosts through core validation
    
    (cherry picked from commit 76a611cf3e0f20fc1bbabda71ccf031c8b016249)
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31431179501
    
    Upstream-Ref: Automattic/jetpack@46fced05297d830b43e72dd6c6d4365c394dc999

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.9.2-alpha1786112572',
+      'ver' => '0.9.2',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.51-alpha1786027392',
+      'ver' => '4.3.51',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.42.0-alpha1786319882',
+      'ver' => '5.42.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.84.4-alpha1785928487',
+      'ver' => '0.84.4',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
@@ -98,7 +98,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.4.0-alpha1785922550',
+      'ver' => '0.4.0',
     ),
   ),
   'paths' => array(
