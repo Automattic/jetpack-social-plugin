0135d6f4bac00ae1ce61395661464a4dc1265cc6
anomiex <anomiex@users.noreply.github.com>
2026-08-19 17:38:36 +0000

    Update dependency concurrently to v10.0.5 (#51399)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32280973760
    
    Upstream-Ref: Automattic/jetpack@dd5eaf7d8427a838ba47ccae19f136b6cd2c5e00

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,11 +14,11 @@ return array(
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.9.4',
+      'ver' => '0.9.5-alpha1787159987',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.10',
+      'ver' => '4.4.11-alpha1787159987',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.43.0',
+      'ver' => '5.43.1-alpha1787159987',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.85.0-alpha1786969720',
+      'ver' => '0.85.0-alpha1787159987',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
