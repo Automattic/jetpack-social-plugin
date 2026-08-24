3f2c0fda6aa7015b776f7642c827748fb9452035
kraftbj <kraftbj@users.noreply.github.com>
2026-08-24 15:22:49 +0000

    Publicize: gate connection script data on the Publicize capability (#51483)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32742638726
    
    Upstream-Ref: Automattic/jetpack@0e55c186b404d2fc65caa445c34598079a94bf19

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.85.1-alpha1787281854',
+      'ver' => '0.85.1-alpha1787583874',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
