b60f7783e2fbb3921acf30bbaeb81bff22da4019
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-09-10 20:05:43 +0000

    Backport Jetpack 16.2 release changes (#52194)
    * SEO: preserve generated metadata handling and package release state
    
    * Related Posts: preserve protected content handling
    
    * External Media: preserve copy request handling
    
    * Forms: preserve published 8.0.2 release metadata
    
    * Prepare Jetpack 16.2 and dependency release versions
    
    * Squash Jetpack 16.2 prerelease changelogs
    
    * Refine Jetpack 16.2 release changelog wording
    
    * Sort Jetpack 16.2 changelog entries
    
    * Update Jetpack 16.2 release readme
    
    * Set Jetpack 16.2 release date to September 10
    
    ---------
    
    Co-authored-by: Chris Jean <chris.jean@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34522394788
    
    Upstream-Ref: Automattic/jetpack@1a58ea305e515f745297c7375d01696371fccf66

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.11.3-alpha1788909266',
+      'ver' => '0.11.4-alpha1789069591',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.2.0-alpha1789036555',
+      'ver' => '9.2.0-alpha1789069591',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.2.1-alpha1789064482',
+      'ver' => '6.2.2-alpha1789069591',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.86.2-alpha1789064482',
+      'ver' => '0.86.3-alpha1789069591',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.5.3-alpha1788902387',
+      'ver' => '0.5.3',
     ),
   ),
   'paths' => array(
