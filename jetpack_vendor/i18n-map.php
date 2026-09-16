cff73281cd96a1f70eb31d514d3a6a992a7c82d4
enejb <enejb@users.noreply.github.com>
2026-09-16 20:00:24 +0000

    My Jetpack: add a flag that replaces the Products tab with a Features tab (#52346)
    * My Jetpack: add a flag that replaces the Products tab with a Features tab
    
    * My Jetpack: publish the products tab slug and label once for the tab, links and footers
    
    * My Jetpack: keep trunk's Products strings while the Features tab is off, and cover the review gaps
    
    * My Jetpack: address review on the Features tab flag
    
    * Jetpack: move the footer products link helper out of Jetpack_Admin_Page
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35141565627
    
    Upstream-Ref: Automattic/jetpack@34903b0292964820f57d1bbe844f9a5873d29883

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.3.0-alpha1789559411',
+      'ver' => '6.3.0-alpha1789587380',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
