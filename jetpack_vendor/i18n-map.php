3890f0d81978bdb4fee216df4cde174bd92b9f14
haqadn <haqadn@users.noreply.github.com>
2026-09-17 19:05:41 +0000

    Backport jetpack 16.3-a.1 changes (#52349)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Update projects/packages/forms/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/connection/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/plugins/jetpack/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/paypal-payments/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35260462577
    
    Upstream-Ref: Automattic/jetpack@5475aa3314d6cda0306c99c4f6bff00636df6950

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.12.0-alpha1789559411',
+      'ver' => '0.12.1-alpha1789670548',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.4-alpha1789575972',
+      'ver' => '5.0.5-alpha1789670548',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.3.0-alpha1789576905',
+      'ver' => '9.3.1-alpha1789670548',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.6.2-alpha1789667613',
+      'ver' => '0.6.3-alpha1789670548',
     ),
     'jetpack-feature-flags' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-feature-flags',
@@ -50,23 +50,23 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '5.0.4-alpha1789559411',
+      'ver' => '5.0.5-alpha1789670548',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '4.1.0-alpha1789426321',
+      'ver' => '4.1.0',
     ),
     'jetpack-menu-badges' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-menu-badges',
-      'ver' => '0.2.1-alpha1789417599',
+      'ver' => '0.2.1',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.3.0-alpha1789645031',
+      'ver' => '6.4.0-alpha1789670548',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.5.1-alpha1789403875',
+      'ver' => '0.5.1',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -74,7 +74,7 @@ return array(
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.10.2-alpha1789559411',
+      'ver' => '0.10.3-alpha1789670548',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
@@ -90,15 +90,15 @@ return array(
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.87.0-alpha1789559411',
+      'ver' => '0.87.1-alpha1789670548',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.1.1-alpha1789504981',
+      'ver' => '5.1.2-alpha1789670548',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.5.5-alpha1789575949',
+      'ver' => '0.5.6-alpha1789670548',
     ),
   ),
   'paths' => array(
