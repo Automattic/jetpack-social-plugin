2c8d309a00e44dad21cfa44183e0b1d7d2289965
chihsuan <chihsuan@users.noreply.github.com>
2026-09-23 07:38:03 +0000

    Sync: Fix WooCommerce Analytics sync fatal when Analytics is turned off (#52681)
    * Sync: Fix WooCommerce Analytics sync fatal when Analytics is turned off
    
    * Sync: Reload plain orders as WooCommerce Analytics orders instead of reading stale rows
    
    * Sync: Load each WooCommerce Analytics order once during full sync
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35830613625
    
    Upstream-Ref: Automattic/jetpack@a464b883c959b8f9bee491387ab5bfbf7e18c813

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -94,7 +94,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.1.3-alpha1790131955',
+      'ver' => '5.1.3-alpha1790147616',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
