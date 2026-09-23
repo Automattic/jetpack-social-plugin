cb6ea355eb412bc7c1636cf88320015265514d3e
kraftbj <kraftbj@users.noreply.github.com>
2026-09-23 15:45:12 +0000

    My Jetpack: rise the feature details dialog into place instead of scaling it (#52590)
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35880825702
    
    Upstream-Ref: Automattic/jetpack@1a8bf41fd54c3274a593ef509dbb9e5fd81b6410

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.6.0-alpha1790176704',
+      'ver' => '6.6.0-alpha1790176826',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
