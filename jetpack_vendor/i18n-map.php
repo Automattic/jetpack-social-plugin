f1bb82ca3fde785a238459ca51d8c5ca723c97d2
enejb <enejb@users.noreply.github.com>
2026-09-24 18:49:33 +0000

    My Jetpack: Add a dismissible banner to the Features tab (#52733)
    * My Jetpack: Add a dismissible banner to the Features tab
    
    * My Jetpack: Match the Features banner title to the tab heading size
    
    * My Jetpack: Remember the Features banner dismissal per user in user meta
    
    * My Jetpack: Keep the Features banner dismissal in the query cache
    
    * My Jetpack: Test the Features banner dismissal and name its close button
    
    * My Jetpack: Drop the covers attribute from the Features banner REST test
    
    * My Jetpack: Report a failed Features banner dismissal instead of claiming success
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/36041738123
    
    Upstream-Ref: Automattic/jetpack@2c5fedce13d78ef0c301ecd55feb769f8409d890

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.7.0-alpha1790271773',
+      'ver' => '6.7.0-alpha1790274638',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
