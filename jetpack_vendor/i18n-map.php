5acd3da5d90134c8e284d11c6d9ca4fe6963f12e
enejb <enejb@users.noreply.github.com>
2026-09-25 18:04:54 +0000

    My Jetpack: Configure beside the switch, and keep Brute Force Protection off the Protect card (#52829)
    * My Jetpack: Rename the More Features settings link to Configure and move it beside the toggle
    
    * My Jetpack: Key the Protect feature to protect-dashboard so Brute Force Protection no longer switches it on
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/36169032425
    
    Upstream-Ref: Automattic/jetpack@56ba432f7afa9c8f9a50b3ccf175f5a0c0baeaec

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.7.0-alpha1790356921',
+      'ver' => '6.7.0-alpha1790358278',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
